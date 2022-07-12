b3482ecf13a680f3efe2360cc2b57650671d386c
juviannrel <106714202+juviannrel@users.noreply.github.com>
2022-07-12 21:17:02 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* Dani Offcial
-*ᴀɢᴇ:* 15ᵗʰ
+*ɴᴀᴍᴇ:* Farell Official 
+*ᴀɢᴇ:* 16ᵗʰ
 *sᴛᴀᴛᴜs:* Hanya Butuh Pasangan:v 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 16 Desember 2007
-*ᴀᴅᴅʀᴇss:* Indonesia, Kalsel, Kandangan
+*ʙɪʀᴛʜᴅᴀʏ:* 03 November 2005
+*ᴀᴅᴅʀᴇss:* Indonesia, Medan, Medan Baru
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
